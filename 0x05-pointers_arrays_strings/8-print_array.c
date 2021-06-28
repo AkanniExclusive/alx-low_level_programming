0723aafe32c8c8f88b61926f707924d520c798b1
AkanniExclusive <olamideakanni2020@gmail.com>
2021-06-28 09:58:22 -0700

    Hot fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,31 +1,28 @@
-#include "holberton.h" 
-#include "stdio.h" 
-#include "string.h" 
- 
-/** 
- * print_array - Print array upto n elements 
- * @a: Array to be read from 
- * @n: Number of elements to be read 
- * Return: Always 0. 
- */ 
-void print_array(int *a, int n) 
-{ 
- int i; 
- 
- 
- if (n > 0) 
- { 
-  for (i = 0; n > i; i++) 
-  { 
-   if (i != n-1) 
-   { 
-    printf("%d, ", a[i]); 
-   } 
-   else 
-   { 
-    printf("%d", a[i]); 
-   } 
-  } 
- } 
- printf("\n"); 
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - print `n` elements of an array of integers
+ * @a: int type array pointer
+ * @n: int type integer
+ * Description: Numbers must be separated by comma and space.
+ * Numbers should be displayed in the same order they are stored in array.
+ * You can only use _putchar to print.
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	for (n--; n >= 0; n--, i++)
+	{
+		printf("%d", a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+
 }
