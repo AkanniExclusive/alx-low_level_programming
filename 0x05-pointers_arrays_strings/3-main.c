a33a471894cdb4a1d2acd27c0566b9e7c971d1b0
AkanniExclusive <olamideakanni2020@gmail.com>
2021-06-28 08:06:37 -0700

    quick change on puts

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -1,17 +1,14 @@
-#include <stdio.h>
-
-void _puts(char *);
+#include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for ALX School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char *str;
-
-	str = "Holberton!";
-	_puts(str);
-	return (0);
+    char *str;
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
+    return (0);
 }
