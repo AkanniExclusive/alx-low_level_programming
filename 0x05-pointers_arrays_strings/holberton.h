ec31905aa1e5f308d727a375e04ef2823f8405bf
AkanniExclusive <olamideakanni2020@gmail.com>
2021-06-28 05:18:16 -0700

    change on putchar

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,5 +1,6 @@
 #ifdef HOLBERTON_H
 #define HOLBERTON_H
+int _putchar(char c);
 extern void reset_to_98(int *n);
 extern void swap_int(int *a, int *b);
 extern int strlen(char *s);
@@ -8,8 +9,8 @@ extern void print_rev(char *s);
 extern void rev_string(char *s);
 extern void puts2(char *str);
 extern void puts_half(char *str);
-extern void _putchar(char c);
 extern void print_array(int *a, int n);
 extern void *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 #endif
 
