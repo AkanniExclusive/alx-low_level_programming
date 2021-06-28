ba87a5b3f843173915432592fa3391b82227097a
AkanniExclusive <olamideakanni2020@gmail.com>
2021-06-28 05:53:35 -0700

    added main

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+void _puts(char *);
+
+/**
+ * main - Check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "Holberton!";
+	_puts(str);
+	return (0);
+}
