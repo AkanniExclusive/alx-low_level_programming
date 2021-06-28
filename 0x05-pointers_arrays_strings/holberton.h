b6e1a16d2015e62cb4ff554ef284b84c0900c9d3
AkanniExclusive <olamideakanni2020@gmail.com>
2021-06-28 09:10:24 -0700

    another fix

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,16 +1,13 @@
-#ifdef HOLBERTON_H
-#define HOLBERTON_H
 int _putchar(char c);
-extern void reset_to_98(int *n);
-extern void swap_int(int *a, int *b);
-extern int strlen(char *s);
-extern void _puts(char *str);
-extern void print_rev(char *s);
-extern void rev_string(char *s);
-extern void puts2(char *str);
-extern void puts_half(char *str);
-extern void print_array(int *a, int n);
-extern void *_strcpy(char *dest, char *src);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-#endif
-
+© 2021 GitHub, Inc.
