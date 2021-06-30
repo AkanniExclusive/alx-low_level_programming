aeedac804540839905cfabc130da24fa7faa6e54
AkanniExclusive <olamideakanni2020@gmail.com>
2021-06-29 17:31:45 -0700

    Nested loops functions

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,41 @@
 #include "holberton.h"
 
 /**
- * add - addes to integer
- * @num1: first integer argument
- * @num2: second integer argument
- * Return: the sum of num1 and num2
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * Return: void
  */
-int add(int num1, int num2)
+void times_table(void)
 {
-	return (num1 + num2);
+	int i = 0;
+	int j = 0;
+	int prod;
+
+	while (i < 10)
+	{
+		while (j < 10)
+		{
+			prod = i * j;
+			if (prod <= 9 && j != 0)
+				_putchar(' ');
+			if (prod > 9)
+			{
+				_putchar(prod / 10 + '0');
+				_putchar(prod % 10 + '0');
+			}
+			else
+			{
+				_putchar(prod + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			j++;
+		}
+		_putchar('\n');
+		i++;
+		j = 0;
+	}
 }
